77e138f6ce1bd9aaa336563e761436de9864feee
vaibhavprashanth <65724191+vaibhavprashanth@users.noreply.github.com>
2022-11-27 00:23:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+ 
+
+import authRoute from "../routes/auth.mjs";
+import usersRoute from "../routes/users.mjs";
+import hotelsRoute from "../routes/hotels.mjs";
+import roomsRoute from "../routes/rooms.mjs";
+const app=express()
+dotenv.config(); 
+
+ 
+const connect=async()=>{try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongoDB disconnected")
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log("mongoDB connected")
+})
+
+//middleware
+
+app.use(express.json())
+
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/rooms",roomsRoute);
+
+app.use((err,req,res,next)=>{
+  const errorStatus= err.status || 500
+  const errorMessage = err.message || "Something went wrong"
+  return res.status(errorStatus).json(
+    {
+      "success":false,
+      status:errorStatus,
+      message:errorMessage,
+      stack:err.stack,
+    });
+});
+
+app.listen(8800, ()=>{
+    connect();
+    console.log("Connected to backend!")
+
+})
